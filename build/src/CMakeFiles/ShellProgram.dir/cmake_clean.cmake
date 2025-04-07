d2d9a353016f25ba5ff0463b3e7a789ad4ac7525
Driss HAMADOU <driss.hamadou12@gmail.com>
2025-04-07 10:11:32 +0200

    Split into (main + read_line) Add "q" command to quit

--- build/src/CMakeFiles/ShellProgram.dir/cmake_clean.cmake
+++ build/src/CMakeFiles/ShellProgram.dir/cmake_clean.cmake
@@ -3,6 +3,8 @@ file(REMOVE_RECURSE
   "../output/bin/ShellProgram.pdb"
   "CMakeFiles/ShellProgram.dir/main.c.o"
   "CMakeFiles/ShellProgram.dir/main.c.o.d"
+  "CMakeFiles/ShellProgram.dir/read_line.c.o"
+  "CMakeFiles/ShellProgram.dir/read_line.c.o.d"
 )
 
 # Per-language clean rules from dependency scanning.
---
+++ src/read_line.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+void read_line(char *input, size_t size) {
+    printf("> ");
+    if (fgets(input, size, stdin) != NULL) {
+        // Remove newline character from input
+        input[strcspn(input, "\n")] = 0;
+    } else {
+        printf("Error reading input.\n");
+        input[0] = '\0'; // Set input to an empty string in case of error
+    }
+}
