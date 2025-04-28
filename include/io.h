d8f06905d9f76f0f2e0c3f50d2f99aa25f01fb4c
Driss HAMADOU <driss.hamadou12@gmail.com>
2025-04-28 20:08:19 +0200

    Copy io.h from Issam's Branch

---
+++ include/io.h
@@ -0,0 +1,11 @@
+#ifndef IO_H
+#define IO_H
+
+#include <unistd.h>
+#include <fcntl.h>
+
+int redirect_input(const char *input_source);
+int redirect_output(const char *output_target);
+
+
+#endif // IO_H
